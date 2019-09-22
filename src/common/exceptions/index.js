23317afd9270bdf1fe1ad2d4f403cee1f0c693be
Giovanni Fiori <giovannicfiori@gmail.com>
2019-09-22 17:52:48 -0400

    refactor on exceptions

--- src/common/exceptions/index.js
+++ src/common/exceptions/index.js
@@ -1,6 +1,40 @@
+const ServerError = require('./ServerError');
+const HttpStatusCodes = require('../util/HttpStatusCodes');
+
+class BadRequestException extends ServerError {
+  constructor(message = 'Parameters are missing') {
+    super(message, HttpStatusCodes.BAD_REQUEST);
+    this.name = 'BadRequestException';
+  }
+}
+
+class NotAuthorizedException extends ServerError {
+  constructor(message = "You're not allowed to perform this action") {
+    super(message, HttpStatusCodes.NOT_AUTHORIZED);
+    this.name = 'NotAuthorizedException';
+  }
+}
+
+class NotFoundException extends ServerError {
+  constructor(message = 'The requested resource was not found') {
+    super(message, HttpStatusCodes.NOT_FOUND);
+    this.name = 'NotFoundException';
+  }
+}
+
+const exceptionHandler = (e, res) => {
+  if (e instanceof ServerError) {
+    return res.status(e.status).json(e);
+  }
+  return res
+    .status(HttpStatusCodes.INTERNAL_SERVER_ERROR)
+    .json(new ServerError(e.message, HttpStatusCodes.INTERNAL_SERVER_ERROR));
+};
+
 module.exports = {
-  BadRequestException: require('./BadRequestException'),
-  NotAuthorizedException: require('./NotAuthorizedException'),
-  NotFoundException: require('./NotFoundException'),
-  ServerError: require('./ServerError')
+  BadRequestException,
+  NotAuthorizedException,
+  NotFoundException,
+  ServerError,
+  exceptionHandler
 };
