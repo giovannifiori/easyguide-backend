852fce02677e44b0f0f2e37168560bf07d724a7b
Giovanni Fiori <giovannicfiori@gmail.com>
2019-09-22 18:22:35 -0400

    endpoint to add a place to user favorites

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const userController = require('../controllers/userController');
+const {
+  getFavoritePlaces,
+  saveNewFavoritePlace
+} = require('../controllers/userController');
 
-router.route('/:id/favorites').get(userController.getFavoritePlaces);
+router
+  .route('/:id/favorites')
+  .get(getFavoritePlaces)
+  .post(saveNewFavoritePlace);
 
 module.exports = router;
