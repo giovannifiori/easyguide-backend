39010750dbd96e4fd0493f14b8dde9af444e8d2f
Giovanni Fiori <giovannicfiori@gmail.com>
2019-09-22 00:11:15 -0400

    fixed environment issues

--- config/config.js
+++ config/config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOSTNAME,
-  dialect: 'postgres'
+  development: {
+    username: 'easyguide',
+    password: 'eg1234',
+    database: 'easyguide_db',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'easyguide',
+    password: 'eg1234',
+    database: 'easyguide_db',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres'
+  }
 };
